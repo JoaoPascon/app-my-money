e2dde9e35c47cf00dbb62e729f8be3204710e0bb
JoaoPascon <joaonatan93@gmail.com>
2018-08-21 20:58:10 -0300

    Seção Créditos

--- frontend/src/billingCycle/billingCycle.jsx
+++ frontend/src/billingCycle/billingCycle.jsx
@@ -8,7 +8,6 @@ import TabsContent from '../common/tab/tabsContent'
 import TabsHeader from '../common/tab/tabsHeader'
 import TabHeader from '../common/tab/tabHeader'
 import TabContent from '../common/tab/tabContent'
-import { selectTab, showTabs } from '../common/tab/tabActions'
 import { create, init, update, remove } from './billingCyclesActions'
 import List from './billingCycleList'
 import Form from './billingCycleForm'
@@ -16,8 +15,7 @@ import Form from './billingCycleForm'
 class BillingCycle extends Component {
 
     componentWillMount() {
-        this.props.selectTab('tabList')
-        this.props.showTabs('tabList', 'tabCreate')
+        this.props.init()
     }
 
     componentWillUnmount(){
@@ -41,13 +39,13 @@ class BillingCycle extends Component {
                                 <List/> 
                             </TabContent>
                             <TabContent id='tabCreate'> 
-                                <Form onSubmit={this.props.create} />
+                                <Form onSubmit={this.props.create} submitLabel="Incluir" submitClass="btn btn-primary"/>
                             </TabContent>
                             <TabContent id='tabUpdate'> 
-                                <Form onSubmit={this.props.update}/>
+                                <Form onSubmit={this.props.update} submitLabel="Alterar" submitClass="btn btn-warning"/>
                              </TabContent>
                             <TabContent id='tabDelete'> 
-                                <Form onSubmit={this.props.remove} readOnly={true}/>
+                                <Form onSubmit={this.props.remove} submitLabel="Excluir" submitClass="btn btn-danger" readOnly={true}/>
                             </TabContent>
                         </TabsContent>
                     </Tabs>
@@ -57,5 +55,5 @@ class BillingCycle extends Component {
     }
 }
 
-const mapDispatchToProps = dispatch => bindActionCreators({selectTab, showTabs, create, init, update, remove }, dispatch)
+const mapDispatchToProps = dispatch => bindActionCreators({create, init, update, remove }, dispatch)
 export default connect(null, mapDispatchToProps)(BillingCycle)
--- frontend/src/billingCycle/billingCycleForm.jsx
+++ frontend/src/billingCycle/billingCycleForm.jsx
@@ -1,31 +1,35 @@
 import React, { Component } from 'react'
-import { reduxForm, Field } from 'redux-form'
+import { reduxForm, Field, formValueSelector } from 'redux-form'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 
-import { init } from './billingCyclesActions'
-import labelAndInput from '../common/form/labelAndInput'
+import { init, } from './billingCyclesActions'
+import LabelAndInput from '../common/form/labelAndInput'
+import CreditList from './creditList'
 
 class BillingCycleForm extends Component {
 
     
     render(){
         
-        const { handleSubmit, readOnly } = this.props
+        const { handleSubmit, readOnly, credits } = this.props
 
         return (
             <form role='form' onSubmit={handleSubmit}>
                 <div className='box-body'>
-                    <Field name='name' component={labelAndInput} readOnly={readOnly}
+                    <Field name='name' component={LabelAndInput} readOnly={readOnly}
                     label='Nome' cols='12 4' placeholder='Informe o nome'/>
-                    <Field name='month' component={labelAndInput} readOnly={readOnly} type="number"
+                    <Field name='month' component={LabelAndInput} readOnly={readOnly} type="number"
                     label='Mês' cols='12 4' placeholder='Informe o Mês'/>
-                    <Field name='year' component={labelAndInput} readOnly={readOnly} type="number"
+                    <Field name='year' component={LabelAndInput} readOnly={readOnly} type="number"
                      label='Ano' cols='12 4' placeholder='Informe o Ano'/>
+                     <CreditList cols='12 6' list={credits} readOnly={readOnly}/>
                 </div>
                 <div className='box-footer'>
-                    <button type="submit" className="btn btn-default">Submit</button>
-                    <button type="button" className="btn btn-warning" onClick={this.props.init}>Cancelar</button>
+                    <button type="submit" className={`${this.props.submitClass}`}>
+                        {this.props.submitLabel}
+                    </button>
+                    <button type="button" className="btn btn-default" onClick={this.props.init}>Cancelar</button>
                 </div>
             </form>
         )
@@ -33,5 +37,8 @@ class BillingCycleForm extends Component {
 }
 
 BillingCycleForm = reduxForm({form: 'billingCycleForm', destroyOnUnmount: false})(BillingCycleForm)
+const selector = formValueSelector('billingCycleForm')
+
+const mapStateToProps = state => ({credits: selector(state, 'credits')})
 const mapDispatchToProps = dispatch => bindActionCreators({init}, dispatch)
-export default connect(null, mapDispatchToProps)(BillingCycleForm)
+export default connect(mapStateToProps, mapDispatchToProps)(BillingCycleForm)
